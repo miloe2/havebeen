5d7f51c1083245b87385af0cfde0127db80657f0
taek <miloe0117@gmail.com>
2024-03-15 01:35:51 +0900

    [Add] Category Swiper & api 호출

--- components/Ui/SwiperSlider.vue
+++ components/Ui/SwiperSlider.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="slider-container relative overflow-hidden">
         <div class="slide-track flex transition-transform duration-500" :style="slideStyle">
-            <div class="slide w-full ring-1 flex-shrink-0" v-for="slide in slides" :key="slide.id">
-                <!-- Slide Content -->
-
-                {{ slide }}
-            </div>
+            <ul class="slide w-full  grid grid-cols-3 flex-shrink-0 gap-x-4" v-for="slides in groupedSlides" :key="slides.id">
+                <li v-for="(item, index) in slides" :key="index"
+                class=" bg-zinc-100 rounded-2xl  gap-x-4 w-full h-40 relative">
+                <div class="absolute top-0 left-0 w-full h-full rounded-2xl">
+                    <img :src="`https://source.unsplash.com/random/1${index}0x200`" alt="" class="w-full h-full object-cover rounded-2xl">
+                </div>
+                <div class="absolute top-0 left-0 bg-black opacity-40 w-full h-full rounded-2xl"></div>
+                <div class=" text-sm text-white absolute top-0 left-0 w-full h-full flex justify-center items-center flex-col">
+                    <div class="font-medium ">{{ item.shortName }}</div>
+                    <div class="">{{ item.eventName }}</div>
+                    <div class="">{{ eventPeried(item.startDate, item.finishDate )}}</div>
+                </div>
+                </li>
+            </ul>
         </div>
         <button @click="handleLeft"><-</button>
         <button @click="handleRight">-></button>
@@ -13,11 +22,21 @@
 </template>
 <script setup>
 import { ref, computed } from 'vue';
+const props = defineProps({
+    itemsList : { type : Array, required : Number},
+    slidesPerPage : {type : Number}
+});
+const groupedSlides = computed(() => {
+    const groups  = [];
+    for(let i = 0; i < props.itemsList.length; i += props.slidesPerPage) {
+        groups.push(props.itemsList.slice(i , i + props.slidesPerPage));
+    }
+    return groups
+});
 
 const currentIndex = ref(0);
-const slides = ref([1,2,3,4,5]); // 슬라이드 데이터
 const handleRight = () => {
-    if(currentIndex.value === slides.value.length - 1){
+    if(currentIndex.value === groupedSlides.value.length - 1){
         return ;
     } else {
         currentIndex.value++;
@@ -35,9 +54,3 @@ const slideStyle = computed(() => ({
     transform: `translateX(-${currentIndex.value * 100}%)`
 }));
 </script>
-
-<style scoped>
-/* 슬라이더 컨테이너 스타일 */
-
-
-</style>
--- components/exhibitions/RelativeExhibitions.vue
+++ components/exhibitions/RelativeExhibitions.vue
@@ -1,6 +1,6 @@
 <template >
-    <div class="flex flex-col w-[900px] bg-yellow-100 mt-4">
-        <h1>{{ props.category }} 전시회를 찾으세요?</h1>
+    <div class="flex flex-col w-[900px] bg-yellow- mt-4">
+        <h1 class="font-bold text-lg">{{ props.category }} 전시회를 찾으세요?</h1>
         <!-- <div v-for="(item, index) in categoryList" :key="index"
         class=" bg-red-50 grid grid-cols-3 w-full h-100">
            <div class="ring-1"> {{ item.eventName }}</div>
@@ -9,8 +9,8 @@
         <!-- <ul  class=" bg-red-50 grid grid-cols-3 w-full gap-4">
             <li v-for="(item, index) in categoryList" :key="index" class="bg-blue-50 w-full h-40 ">{{ item.eventName }}</li>
         </ul> -->
-        <div class="w-full">
-            <SwiperSlider/>
+        <div class="w-full mt-4">
+            <SwiperSlider :itemsList="categoryList" :slidesPerPage="3"/>
 
         </div>
     </div>
